38928829c4297f8a07a9d6bf19e21a39a39ce949
AnJia <anjia0532@gmail.com>
2018-06-06 00:51:52 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,38 +13,42 @@ Google Container Registry Mirror [last sync 2018-06-05 12:28 UTC]
 
 Syntax
 -------
-```bash
+
+\`\`\`bash
 gcr.io/namespace/image_name:image_tag eq ${user_name}/namespace.image_name:image_tag
-```
+\`\`\`
+
 Example
 -------
 
-```bash
+\`\`\`bash
 docker pull gcr.io/google-containers/federation-controller-manager-arm64:v1.3.1-beta.1 
 # eq 
 docker pull ${user_name}/google-containers.federation-controller-manager-arm64:v1.3.1-beta.1
-```
+\`\`\`
 
 ReTag ${user_name} images to gcr.io 
 -------
-```bash
+
+\`\`\`bash
 # replace gcr.io/google-containers/federation-controller-manager-arm64:v1.3.1-beta.1 to real image
 # this will convert gcr.io/google-containers/federation-controller-manager-arm64:v1.3.1-beta.1 
 # to ${user_name}/google-containers.federation-controller-manager-arm64:v1.3.1-beta.1 and pull 
-eval $(echo $(cat <<EOF
+eval \$(echo \$(cat <<EOF
 gcr.io/google-containers/federation-controller-manager-arm64:v1.3.1-beta.1
 gcr.io/google-containers/federation-controller-manager-arm64:v1.3.1-beta.1
 gcr.io/google-containers/federation-controller-manager-arm64:v1.3.1-beta.1
 EOF
-)| sed 's/gcr\.io/${user_name}/g;s/\//\./g;s/ /\n/g;s/${user_name}\./${user_name}\//g' | uniq | awk '{print "docker pull "$1";"}')
+)| sed 's/gcr\.io/${user_name}/g;s/\//\./g;s/ /\n/g;s/${user_name}\./${user_name}\//g' | uniq | awk '{print "docker pull "\$1";"}')
 
 # this code will retag all of ${user_name}'s image from local  e.g. ${user_name}/google-containers.federation-controller-manager-arm64:v1.3.1-beta.1 
 # to gcr.io/google-containers/federation-controller-manager-arm64:v1.3.1-beta.1
-eval $(docker images | grep ${user_name} | awk '{print $1":"$2}' |awk -F'[/.:]' '{printf "docker tag %s/%s.%s:%s gcr.io/%s/%s:%s;\n",$1,$2,$3,$4,$2,$3,$4}')
-```
+eval \$(docker images | grep ${user_name} | awk '{print \$1":"\$2}' |awk -F'[/.:]' '{printf "docker tag %s/%s.%s:%s gcr.io/%s/%s:%s;\n",\$1,\$2,\$3,\$4,\$2,\$3,\$4}')
+\`\`\`
 
 [Changelog](./CHANGES.md)
 -------
+
 EOT
 
 # create changelog md
@@ -57,20 +61,17 @@ if [ ! -f CHANGES.md ]; then
     touch CHANGES.md
 fi
 
-ns=$(cat gcr_namespaces 2>/dev/null || echo google-containers)
+ns=$(cat ./gcr_namespaces 2>/dev/null || echo google-containers)
 
 for n in ${ns[@]}  ; do
 
     # get all of the gcr images
     imgs=$(curl -ks 'https://console.cloud.google.com/m/gcr/entities/list'  -H 'cookie: SID=WgX93aiB6sVpD_FPLDBsPHvLnYdhtMXYt9bHsf_TmrmIvLkrnc11D84pIcS-3WB9fYIHKw.; HSID=A--M5SxveLfh2e7Jl; SSID=AqvfThGwBO94ONF2d; OSID=ZAX93cIEBWYq35v3hq6J5U3MNU3voHihnEqmrmIirWBfHluQ3Gjbb4E24vDuPoSVKpC2tg.'  -H 'content-type: application/json;charset=UTF-8'   --data-binary '["'${n}'"]' | grep -P '"' | sed 's/"gcr.ListEntities"//'|cut -d '"' -f2 |sort|uniq)
-	cat <<EOT >> gcr.io_mirror/README.md
 
-	Total of $(echo ${imgs[@]} | grep -o ' ' | wc -l)'s gcr.io/${n}/* images
-	-------
-
-	EOT
+	echo -e "Total of $(echo ${imgs[@]} | grep -o ' ' | wc -l)'s gcr.io/${n}/* images\n\n-------\n\n" >> gcr.io_mirror/README.md
+	
     # remove all of the imgs tmp file
-    rm -rf "gcr.io_mirror/${n}/*"
+    rm -rf "./gcr.io_mirror/${n}/*"
 
     for img in ${imgs[@]}  ; do
         # get all  tags for this image
